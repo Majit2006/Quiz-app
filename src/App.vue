92adef66837580c2c2cda4b3a9005a41034fb82f
Majit2006 <majitabbasow85@gmail.com>
2022-06-14 15:51:59 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <quiz-app />
+  </div>
+</template>
+<script>
+import QuizApp from "./components/quiz-app.vue";
+export default {
+  components: { QuizApp },
+};
+</script>
+<style>
+@import url('./assets/bootstrap.css');
+</style>
---
+++ src/components/quiz-app.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div class="d-flex justify-content-center flex-wrap m-3" v-if="count < 20">
+      <div class="card col-md-5">
+        <div class="card-header bg-dark text-light  text-center">{{ questions[count].category }}</div>
+        <div class="card-body bg-dark text-light">
+          <h5 class="card-title text-center">{{ questions[count].question }}</h5>
+          <ul class="list-group ">
+            <li class="list-group-item bg-dark text-light border-light"
+              v-for="(option, index) in questions[count].incorrectAnswers " :key="index.id">
+              <label for="radio">
+                <input class="form-check-input me-1" type="radio" id="radio" name="radio" checked>
+                 {{ option }}
+                {{ index }}
+              </label>
+            </li>
+          </ul>
+          <button type="submit" class="btn btn-primary mt-4" @click="++count">Next</button>
+        </div>
+
+      </div>
+      {{ count }}
+    </div>
+    <div class="card col-md-5 d-flex justify-content-center  algin-item-center m-auto mt-4 bg-dark text-light" v-else>
+      <div class="card-body">
+        <h1 class="card-title text-center border-bottom pb-2">Quiz Result</h1>
+        <div class="answers">
+          <p>correct answers:{{ correctAnswers }}</p>
+          <p>wrong answers:{{ wrongAnswers }}</p>
+        </div>
+        <div class="answers-btn border-top">
+          <a href="/" class="btn btn-sm btn-primary mt-4">Go To Home</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      correctAnswers: 0,
+      wrongAnswers: 0,
+      count: 0,
+      number: 1,
+      questionsLength: 20,
+      questions: [],
+    };
+  },
+  mounted() {
+    axios
+      .get("https://the-trivia-api.com/api/questions?categories=arts_and_literature,food_and_drink,film_and_tv,general_knowledge,geography,history,music,science,society_and_culture,sport_and_leisure&limit=20&difficulty=hard")
+      .then(response => {
+        this.questions = response.data;
+        this.questions.forEach((question) => {
+          question.incorrectAnswers.push(question.correctAnswer)
+        })
+        console.log(response.data);
+      })
+  },
+
+};
+</script>
+
+
+<style>
+[type="radio"] {
+  box-shadow: none !important;
+}
+
+.answers {
+  display: flex;
+  text-transform: capitalize;
+  flex-direction: column;
+}
+
+.answers-btn {
+  font-size: 20px;
+}
+</style>
